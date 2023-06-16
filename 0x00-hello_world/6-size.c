c3b82ae8b6539e8a53c0221a554209907c57101c
eliasmereke <babibebshu@gmail.com>
2023-06-16 12:53:14 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,6 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
 printf("Size of a char: %d 1 byte(s)\n", sizeof(a));
 printf("Size of a int: %d 1 byte(s)\n", sizeof(b));
 printf("Size of a long int: %d 1 byte(s)\n", sizeof(c));
