edab5a9bb353ed1b6f036385e1e6e423afbcc284
eliasmereke <babibebshu@gmail.com>
2023-06-18 12:47:41 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
- * description: print a quote
- * return: 1 (error)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-char quo[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
-write(1.quo,59);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
